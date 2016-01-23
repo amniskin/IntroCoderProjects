35e364273dde8288489a5444d4c841395921322f
Aaron Niskin <amniskin@gmail.com>
2016-01-22 21:44:07 -0500

    getting on the same page here

--- node/lecture7.js
+++ node/lecture7.js
@@ -17,6 +17,10 @@ http.createServer(function(request, response) {
   var respString = "";
   if(dat.lifter && (dat["supa"] != "")) {
     respString = "Hell yeah, " + dat.fname + "! We're totally going to get swole together doing some " + dat.supa + "!";
+  } else if(dat.lifter) {
+    respString = "That's cool, " + dat.fname + ". I'm a lifter too. Hit me up on Myspace. I post like 7 pictures a day during my workouts, which take up my entire day. It's pure motivation, bro!";
+  } else {
+    respString = dat.lname + "! You need to get in that gym, bro! How are people gonna know about your gains if you don't post pictures all the time on Instagram?!";
   }
   response.writeHead(200, {"Content-Type": "text/plain"});
   response.write("Hello world (AKA, my gym)\n" + respString);
