4b167b61d3dfdf78ad7c12f568059373d6360491
Aaron Niskin <amniskin@gmail.com>
2016-01-22 21:30:10 -0500

    Simple node stuff

--- node/lecture7.js
+++ node/lecture7.js
@@ -2,7 +2,25 @@ var http = require('http'),
     port = 1234;
 
 http.createServer(function(request, response) {
+  var url = request.url;
+  var tempStr = "";
+  for (var i = 2; i<url.length; i++) {
+    tempStr += url[i];
+  }
+  var tempArray = tempStr.split("&");
+  var tempArray2 = tempArray.map(function(thing) {return thing.split("=");});
+  var dat = {};
+  for (var i = 0; i<tempArray2.length; i++) {
+    dat[tempArray2[i][0]] = tempArray2[i][1];
+  }
+
+  var respString = "";
+  if(dat.lifter && (dat["supa"] != "")) {
+    respString = "Hell yeah, " + dat.fname + "! We're totally going to get swole together doing some " + dat.supa + "!";
+  }
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("Hello world (AKA gym)");
+  response.write("Hello world (AKA, my gym)\n" + respString + dat);
   response.end();
+  console.log(dat);
 }).listen(port);
+console.log('Server listening on port ' + port);
