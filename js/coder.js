315da46c3a16bdf3e537d0ac7a7858213d20b2d9
Aaron Niskin <amniskin@gmail.com>
2016-02-04 22:09:57 -0500

    In progress... Fixed clickable buttons on landing page (mobile hidden)

--- js/coder.js
+++ js/coder.js
@@ -7,5 +7,12 @@ $(document).ready(function() {
       $(this).addClass('blur');
     }
   });
+  $(document).keydown(function() {
+    if (event.keyCode == 74 || event.keyCode == 83 || event.keyCode == 40) { // J or S or DownArrow
+      $('body').scrollTop($('body').scrollTop() + 40);
+    } else if (event.keyCode == 75 || event.keyCode == 87) {
+      $('body').scrollTop($('body').scrollTop() - 40);
+    }
+  });
 });
 
